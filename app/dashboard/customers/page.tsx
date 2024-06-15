a2da202aaa1f268e5aedae325680705ef70ed3a3
Samir Cunha <samircunha98@gmail.com>
2024-06-15 12:02:29 -0300

    Add files via upload

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>Customers page works!</h1>
+  )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <h1>
+      Testando rota do dashboard.
+    </h1>
+  )
+}
